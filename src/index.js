bb96576b142558e02f5cf8c0f928d76e4544bbbf
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-02-22 14:33:01 -0700

    index.js and remove browser console warnings

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import { BrowserRouter } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers/root-reducer'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/root-reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const middleware = applyMiddleware(logger);
-const store = createStore(rootReducer, devTools, middleware, applyMiddleware(thunk))
+const store = createStore(rootReducer, devTools, middleware, applyMiddleware(thunk));
+
 ReactDOM.render(
-<Provider store={store}>
-  <App />
-</Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();

0352c4f5371c0ade063ff08b609ecaa60be065c5
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-02-22 15:00:24 -0700

    test reducers, pokemon type

--- src/reducers/test.js
+++ src/reducers/test.js
@@ -7,4 +7,12 @@ describe( 'pokemonTypes', () => {
     const action = actions.addPokemonTypes;
     expect(pokemonTypes([], action)).toEqual(expected);
   });
+
+  it('should return state with array of pokemon types', () => {
+    const mockPokemonTypes = [1, 2, 3];
+    const action = actions.addPokemonTypes(mockPokemonTypes);
+    const expected = mockPokemonTypes;
+
+    expect(pokemonTypes([], action)).toEqual(expected);
+  });
 });

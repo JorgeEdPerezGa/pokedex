1b857582b92cd1bee09d919dfbc1c124040ca3aa
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-02-22 15:33:40 -0700

    test reducers, pokemon group

--- src/reducers/test.js
+++ src/reducers/test.js
@@ -1,4 +1,5 @@
 import { pokemonTypes } from './pokemonTypes';
+import { pokemonGroup } from './pokemonGroup';
 import * as actions from '../actions';
 
 describe( 'pokemonTypes', () => {
@@ -14,5 +15,21 @@ describe( 'pokemonTypes', () => {
     const expected = mockPokemonTypes;
 
     expect(pokemonTypes([], action)).toEqual(expected);
+  });
+});
+
+describe('pokemonGroup', () => {
+  it('should return a defaut state', () => {
+    const expected = [];
+    const action = actions.addPokemonGroup;
+    expect(pokemonGroup([], action)).toEqual(expected);
+  });
+
+  it('should return a state with an array of specific pokemon', () => {
+    const mockGroup = [1, 2, 3];
+    const action = actions.addPokemonGroup(mockGroup);
+    const expected = mockGroup;
+
+    expect(pokemonGroup([], action)).toEqual(expected);
   });
 });
